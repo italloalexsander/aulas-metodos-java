05e0854ead6df32357c7d4277a9f56894a9da6f0
Itallo <italloalexsander@gmail.com>
2022-04-28 16:57:41 -0300

    Aula 2

--- src/aula/dois/Quadrilatero.java
+++ src/aula/dois/Quadrilatero.java
@@ -11,4 +11,7 @@ public class Quadrilatero {
     public static void calculaArea(double baseMenor, double baseMaior, double altura){
         System.out.println("Área do trapézio é: " + ((baseMenor+baseMaior)*altura)/2);
     }
+    public static void calculaArea(float diagonal1, float diagonal2){
+        System.out.println("Área do losango é: " + (diagonal1*diagonal2)/2);
+    }
 }
--- src/aula/dois/Sobrecarga.java
+++ src/aula/dois/Sobrecarga.java
@@ -3,8 +3,9 @@ package aula.dois;
 public class Sobrecarga {
     public static void main(String[] args) {
         Quadrilatero.calculaArea(2);
-        Quadrilatero.calculaArea(2, 3);
+        Quadrilatero.calculaArea(2d, 3d);
         Quadrilatero.calculaArea(2,4,2);
+        Quadrilatero.calculaArea(2f, 3f);
     }
 
 }
